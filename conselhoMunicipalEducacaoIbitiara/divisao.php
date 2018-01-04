61bf1d901c64e95d0d2d6cf22e46b53ee26371cf
Neto Sales <contato@netosales.com.br>
2018-01-04 16:44:48 -0300

    preparacao de arquivos para reunião Conselho Municipal de Educacao.

---
+++ conselhoMunicipalEducacaoIbitiara/divisao.php
@@ -0,0 +1,43 @@
+<?php
+echo "PORTARIA NR 06 DE 20 DE DEZEMBRO DE 2017.";
+
+
+CAPÍTULO 1 {
+
+	Seção 1
+
+		Subseção 1
+
+
+		Subseção 2
+
+
+		Subseção 3
+
+
+		Subseção 4
+
+
+	Seção 2
+
+
+
+	Seção 3
+
+}
+CAPÍTULO 2
+
+CAPÍTULO 3
+
+CAPÍTULO 4
+
+CAPÍTULO 5
+
+CAPÍTULO 6
+
+	Seção 1
+
+	Seção 2
+
+CAPÍTULO 7
+?>
